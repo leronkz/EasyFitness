3d0883677afec99cd42340805b89fb09579bf1b4
Mateusz Owsiak <41147218+leronkz@users.noreply.github.com>
2024-02-22 10:33:10 +0100

    Fix i18n

--- Easy.Fitness/Easy.Fitness.Web/ClientApp/src/i18n.tsx
+++ Easy.Fitness/Easy.Fitness.Web/ClientApp/src/i18n.tsx
@@ -2,6 +2,7 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import Backend from 'i18next-http-backend';
+import dayjs from 'dayjs';
 
 i18n
   .use(Backend)
@@ -10,9 +11,15 @@ i18n
   .init({
     debug: true,
     fallbackLng: 'pl',
+    react: {
+      useSuspense: false,
+    },
     interpolation: {
-      
+      escapeValue: false,
     }
+  })
+  .then(() => {
+    dayjs.locale(i18n.language);
   });
 
 export default i18n;
